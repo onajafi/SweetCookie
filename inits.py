f24d48272d94ff6debef6989a2ee4626fd308686
dot_blue <onajafi141@gmail.com>
2018-09-22 08:51:10 +0330

    Added the inits.py

---
+++ inits.py
@@ -0,0 +1,9 @@
+# This file contains the classified settings of the bot...
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import telebot
+
+tel_token = "<###THE BOTS TOKEN###>" #The bots token which was received from BotFather
+
+bot = telebot.TeleBot(tel_token)
