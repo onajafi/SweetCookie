04eeb5a65ce3bf738c9102751f7bbd0a99780719
dot_blue <onajafi141@gmail.com>
2019-05-07 13:21:49 +0430

    Just a few changes in the message, but there is still a lot to do for ramazan

--- python/sweetCookie.py
+++ python/sweetCookie.py
@@ -190,9 +190,9 @@ def TUESDAY_ALARM():
             for tmpUserID in users.users_book.keys():
                 print "++=",tmpUserID
                 try:
-                    if(tmpUserID not in users.users_auto_res_days.keys()):# Make sure this guy doesn't have an auto_res
+                    if(True or (tmpUserID not in users.users_auto_res_days.keys())):# Make sure this guy doesn't have an auto_res
                         if(users.users_book[tmpUserID]["user"] != None and users.users_book[tmpUserID]["pass"] != None): # If there was some password to get in
-                            bot.send_message(tmpUserID,"وقت رزرو شده...",reply_markup = MSGs.reserve_time_markup)
+                            bot.send_message(tmpUserID,"وقت رزرو شده...(داریم روی رزرو افطار و سحری کار میکنیم، تا اون موقع مستقیم از سایت رزرو کنید...)",reply_markup = MSGs.reserve_time_markup)
                             counter += 1
                     else:
                         auto_res_count += 1
